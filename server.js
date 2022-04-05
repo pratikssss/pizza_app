5d594ca8d4ce53fd681cf2f787dc56f4c5811db0
unknown <pratikm467@gmail.com>
2022-04-05 15:16:47 +0530

    added cart page

--- server.js
+++ server.js
@@ -4,17 +4,25 @@ const ejs = require("ejs");
 const path = require("path");
 const expresslayout = require("express-ejs-layouts");
 const PORT = process.env.port || 3000;
-app.get("/", function (req, res) {
-    res.render("home");
-});
-
-
 // assets
 app.use(express.static('public'));
 // set template engine
 app.use(expresslayout);
 app.set('views', path.join(__dirname, '/resources/views'));
 app.set('view engine', 'ejs');
+app.get("/", function (req, res) {
+    res.render("home");
+});
+
+app.get("/cart", function (req, res) {
+    res.render("customers/cart");
+});
+app.get("/login", function (req, res) {
+    res.render("auth/login");
+});
+app.get("/register", function (req, res) {
+    res.render("auth/register");
+});
 app.listen(PORT, function () {
-    console.log(' Started on port 3000 ');
+    console.log('Started on port 3000 ');
 });
